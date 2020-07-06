54f47a9671e31b9c756d328409e98648fbde19a3
Johannes Ewald <johannes.ewald@peerigon.com>
2020-07-06 14:20:42 +0200

    Update support/index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,6 @@
 const {locale} = new Intl.DateTimeFormat().resolvedOptions();
 
-if (locale !== 'en-US') {
+if (locale !== 'de-AT') {
     throw new Error(
         `The test environment does not provide the expected locale setup: ${JSON.stringify(
             {locale},
