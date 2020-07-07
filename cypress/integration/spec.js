3bc51ec388a5c20b80db9cb6e3e78cc93e9c8c13
Johannes Ewald <johannes.ewald@peerigon.com>
2020-07-08 00:31:19 +0200

    Add demo of Emulation.setLocaleOverride and Emulation.setTimezoneOverride

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,21 @@
-/// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+it("should set the expected locale and timezone", () => {
+  return Cypress.automation("remote:debugger:protocol", {
+    command: "Emulation.setLocaleOverride",
+    params: {
+      locale: "de-AT",
+    },
   })
-})
+    .then(() => {
+      return Cypress.automation("remote:debugger:protocol", {
+        command: "Emulation.setTimezoneOverride",
+        params: {
+          timezoneId: "Pacific/Fiji",
+        },
+      });
+    })
+    .then(() => {
+      const { locale, timeZone } = new Intl.DateTimeFormat().resolvedOptions();
+
+      console.log(locale, timeZone);
+    });
+});
